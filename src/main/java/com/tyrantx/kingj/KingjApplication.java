2e4bd745375b11527938f5bfdd28401642fee730
KingJ <501315555@qq.com>
2019-02-03 23:08:01 +0800

    Initial Commit
    今天没敲什么有用的代码 怠惰啊~~

---
+++ src/main/java/com/tyrantx/kingj/KingjApplication.java
@@ -0,0 +1,14 @@
+package com.tyrantx.kingj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingjApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/tyrantx/kingj/KingjApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tyrantx.kingj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KingjApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
